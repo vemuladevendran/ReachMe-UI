2a65ffce0ac5c20f06fab28b82af3c3f44ee6a69
devendran <devendranvemula@gmail.com>
2021-04-26 11:45:41 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
