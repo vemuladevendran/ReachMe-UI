69046d91e1536bc35e2cb1f159df3f52ba5c3c3a
devendran <devendranvemula@gmail.com>
2021-05-18 21:53:12 +0530

    form added

--- src/app/page/students/reports-form/reports-form.component.ts
+++ src/app/page/students/reports-form/reports-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { StudentService } from 'src/app/services/students/student.service';
 
 @Component({
   selector: 'app-reports-form',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports-form.component.css']
 })
 export class ReportsFormComponent implements OnInit {
-
-  constructor() { }
+  reportForm: FormGroup;
+  data: any;
+  student: any;
+  constructor(
+    private fb: FormBuilder,
+    private studentServ: StudentService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+    this.reportForm = this.fb.group({
+      studentName: ['', [Validators.required]],
+      rollNumber: ['', [Validators.required]],
+      reportTitle: ['', [Validators.required]],
+      reportDate: ['', [Validators.required]],
+      reportContent: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
+    this.getStudentDetails();
+  }
+
+  async getStudentDetails(): Promise<void> {
+
+    try {
+
+      this.data = await this.studentServ.getStudent();
+
+      this.student = this.data.find((student: any) =>
+        student._id === this.route.snapshot.paramMap.get('id'));
+
+      this.reportForm.get('studentName')?.setValue(this.student.firstName);
+      this.reportForm.get('rollNumber')?.setValue(this.student.rollNumber);
+
+    } catch (error) {
+      console.error(error);
+    }
   }
 
 }
